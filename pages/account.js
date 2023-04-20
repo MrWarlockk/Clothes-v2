55c6936403d7155361cfeb7b8750840be6e21ac9
MrWarlockk <gnatmaciej@gmail.com>
2023-04-20 15:33:35 +0200

    Account page, clothes page
    Finished the visuals of the account page, started working on the visuals
    of the clothes page

---
+++ pages/account.js
@@ -0,0 +1,171 @@
+export default function MyAccount() {
+  return (
+    <>
+      <head>
+        <style>
+          @import
+          url('https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap');
+        </style>
+
+        <title> Account page </title>
+      </head>
+      <body>
+        <div className="background_circle1"></div>
+        <div className="background_circle2"></div>
+        <div className="background_line1"></div>
+        <div className="background_line2"></div>
+        <div className="background_line3"></div>
+        <div className="page_wrapper">
+          <div className="login_background">
+            <div className="login_rectangle">
+              <div className={"flexbox_username"}>
+                <div
+                  className={
+                    "flexbox_login_item1" + " " + "flexbox_username_text"
+                  }
+                >
+                  <p>Username</p>
+                </div>
+                <div
+                  className={
+                    "flexbox_login_item12" + " " + "flexbox_username_rectangle"
+                  }
+                >
+                  <p>admin</p>
+                </div>
+              </div>
+              <div className={"flexbox_password"}>
+                <div
+                  className={
+                    "flexbox_login_item2" + " " + "flexbox_password_text"
+                  }
+                >
+                  <p>Password</p>
+                </div>
+                <div
+                  className={
+                    "flexbox_login_item22" + " " + "flexbox_password_rectangle"
+                  }
+                >
+                  <p>*****</p>
+                </div>
+              </div>
+              <div className={"flexbox_options"}>
+                <div
+                  className={
+                    "flexbox_login_item3" + " " + "flexbox_options_text1"
+                  }
+                >
+                  <p>Reset Password</p>
+                </div>
+                <div
+                  className={
+                    "flexbox_login_item3" + " " + "flexbox_options_text2"
+                  }
+                >
+                  <p>Change E-mail</p>
+                </div>
+              </div>
+              <div className={"flexbox_email"}>
+                <div
+                  className={"flexbox_login_item4" + " " + "flexbox_email_text"}
+                >
+                  <p>E-mail</p>
+                </div>
+                <div
+                  className={
+                    "flexbox_login_item42" + " " + "flexbox_email_rectangle"
+                  }
+                >
+                  <p>admin@mail.com</p>
+                </div>
+                <div className={"flexbox_login_item43"}></div>
+              </div>
+              <div
+                className={
+                  "flexbox_login_item5" + " " + "flexbox_edit_rectangle"
+                }
+              >
+                <p>EDIT</p>
+              </div>
+              <div
+                className={
+                  "flexbox_login_item52" + " " + "flexbox_edit_rectangle"
+                }
+              ></div>
+              <div className={"flexbox_email2"}></div>
+              <div className={"flexbox_login_item6"}></div>
+              <div className={"flexbox_login_item7"}></div>
+              <div className={"flexbox_end_text"}>
+                <div className={"flexbox_login_item8"}>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Mauris iaculis molestie augue ut gravida. Nunc lectus massa,
+                    pretium et elit in, dapibus pharetra metus. Vestibulum non
+                    tempor nibh. Phasellus tempus gravida pharetra. Fusce ornare
+                    elit eros, in sollicitudin magna laoreet pharetra. Duis sed
+                    tortor sit amet nibh ultricies tincidunt nec eget augue.
+                    Fusce maximus vulputate massa, sed bibendum libero. Nullam
+                    erat lectus, volutpat sed felis ut, volutpat euismod felis.
+                    Fusce non arcu sed lorem ultrices sollicitudin.
+                  </p>
+                </div>
+                <div className={"rules_text" + " " + "flexbox_login_item8"}>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Mauris iaculis molestie augue ut gravida. Nunc lectus massa,
+                    pretium et elit in, dapibus pharetra metus. Vestibulum non
+                    tempor nibh. Phasellus tempus gravida pharetra. Fusce ornare
+                    elit eros, in sollicitudin magna laoreet pharetra. Duis sed
+                    tortor sit amet nibh ultricies tincidunt nec eget augue.
+                    Fusce maximus vulputate massa, sed bibendum libero. Nullam
+                    erat lectus, volutpat sed felis ut, volutpat euismod felis.
+                    Fusce non arcu sed lorem ultrices sollicitudin.
+                  </p>
+                </div>
+                <div className={"rules_text" + " " + "flexbox_login_item8"}>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Mauris iaculis molestie augue ut gravida. Nunc lectus massa,
+                    pretium et elit in, dapibus pharetra metus. Vestibulum non
+                    tempor nibh. Phasellus tempus gravida pharetra. Fusce ornare
+                    elit eros, in sollicitudin magna laoreet pharetra. Duis sed
+                    tortor sit amet nibh ultricies tincidunt nec eget augue.
+                    Fusce maximus vulputate massa, sed bibendum libero. Nullam
+                    erat lectus, volutpat sed felis ut, volutpat euismod felis.
+                    Fusce non arcu sed lorem ultrices sollicitudin.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="sidebar_img">
+          <img
+            src={"sidebar/rectangle 161.svg"}
+            alt="menu"
+            style={{ height: "90vh" }}
+          />
+        </div>
+
+        <div className="sidebar">
+          <div className={"sidebar_menu" + " " + "sidebar_top"}>
+            <img src={"sidebar/menu.svg"} alt="menu"></img>
+          </div>
+          <div className={"sidebar_clothes" + " " + "sidebar_top"}>
+            <img src={"sidebar/wieszak.svg"} alt="menu"></img>
+          </div>
+          <div className={"sidebar_cupboard" + " " + "sidebar_top"}>
+            <img src={"sidebar/szafa.svg"} alt="menu"></img>
+          </div>
+          <div className={"sidebar_account" + " " + "sidebar_top"}>
+            <img src={"sidebar/moje konto.svg"} alt="menu"></img>
+          </div>
+          <div className={"sidebar_logout" + " " + "sidebar_bottom"}>
+            <img src={"sidebar/wyloguj.svg"} alt="menu"></img>
+          </div>
+        </div>
+      </body>
+    </>
+  );
+}
---
+++ pages/cupboard.js
@@ -0,0 +1,381 @@
+export default function Cupboard() {
+  return (
+    <>
+      <head>
+        <style>
+          @import
+          url('https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap');
+        </style>
+
+        <title> Cupboard page </title>
+      </head>
+      <body>
+        <div className="background_line1"></div>
+        <div className="background_line2"></div>
+        <div className="background_line3"></div>
+
+        <div className="top_cupboard">
+          <div className="filters_cupboard_top">
+            <img
+              src="cupboard/Icon material-filter-list.svg"
+              alt="filters"
+            ></img>
+          </div>
+          <div className="single_pieces_cupboard_top">
+            <p>SINGLE PIECES</p>
+          </div>
+          <div className="outfits_cupboard_top">
+            <div className="outfits_cupboard_top_text">
+              <p>OUTFITS</p>
+            </div>
+          </div>
+          <div className="add_cupboard_top">
+            <img src="cupboard/Union 1.svg" alt="filters"></img>
+          </div>
+        </div>
+
+        <div className="cupboard_all_flexbox_middle">
+          <div className="cupboard_flexbox_middle">
+            <div className="cupboard_single_flexbox_middle_first">
+              <img
+                src={"images/hoodie.png"}
+                alt="hoodie"
+                style={{
+                  height: "27.5vh",
+                  width: "19vw",
+                  borderTopLeftRadius: "15%",
+                  borderTopRightRadius: "15%",
+                }}
+              ></img>
+            </div>
+            <div className="cupboard_single_flexbox_middle_second">
+              <div className="cupboard_flexbox_name_date">
+                <div className="cupboard_single_name">
+                  <p>Lorem Ipsum</p>
+                </div>
+                <div className="cupboard_single_date">
+                  <p>01.01.2023</p>
+                </div>
+              </div>
+              <div className="cupboard_single_heart">
+                <img
+                  src={"cupboard/Icon awesome-heart.svg"}
+                  alt="heart"
+                  style={{ height: "3.5vh", width: "3.5vh" }}
+                ></img>
+              </div>
+            </div>
+            <div className="cupboard_single_flexbox_middle_third">
+              <div className="delete_single_flexbox">
+                <p>DELETE</p>
+              </div>
+              <div className="edit_single_flexbox">
+                <p>EDIT</p>
+              </div>
+            </div>
+          </div>
+          <div className="cupboard_flexbox_middle">
+            <div className="cupboard_single_flexbox_middle_first">
+              <img
+                src={"images/hoodie.png"}
+                alt="hoodie"
+                style={{
+                  height: "27.5vh",
+                  width: "19vw",
+                  borderTopLeftRadius: "15%",
+                  borderTopRightRadius: "15%",
+                }}
+              ></img>
+            </div>
+            <div className="cupboard_single_flexbox_middle_second">
+              <div className="cupboard_flexbox_name_date">
+                <div className="cupboard_single_name">
+                  <p>Lorem Ipsum</p>
+                </div>
+                <div className="cupboard_single_date">
+                  <p>01.01.2023</p>
+                </div>
+              </div>
+              <div className="cupboard_single_heart">
+                <img
+                  src={"cupboard/Icon awesome-heart.svg"}
+                  alt="heart"
+                  style={{ height: "3.5vh", width: "3.5vh" }}
+                ></img>
+              </div>
+            </div>
+            <div className="cupboard_single_flexbox_middle_third">
+              <div className="delete_single_flexbox">
+                <p>DELETE</p>
+              </div>
+              <div className="edit_single_flexbox">
+                <p>EDIT</p>
+              </div>
+            </div>
+          </div>
+          <div className="cupboard_flexbox_middle">
+            <div className="cupboard_single_flexbox_middle_first">
+              <img
+                src={"images/hoodie.png"}
+                alt="hoodie"
+                style={{
+                  height: "27.5vh",
+                  width: "19vw",
+                  borderTopLeftRadius: "15%",
+                  borderTopRightRadius: "15%",
+                }}
+              ></img>
+            </div>
+            <div className="cupboard_single_flexbox_middle_second">
+              <div className="cupboard_flexbox_name_date">
+                <div className="cupboard_single_name">
+                  <p>Lorem Ipsum</p>
+                </div>
+                <div className="cupboard_single_date">
+                  <p>01.01.2023</p>
+                </div>
+              </div>
+              <div className="cupboard_single_heart">
+                <img
+                  src={"cupboard/Icon awesome-heart.svg"}
+                  alt="heart"
+                  style={{ height: "3.5vh", width: "3.5vh" }}
+                ></img>
+              </div>
+            </div>
+            <div className="cupboard_single_flexbox_middle_third">
+              <div className="delete_single_flexbox">
+                <p>DELETE</p>
+              </div>
+              <div className="edit_single_flexbox">
+                <p>EDIT</p>
+              </div>
+            </div>
+          </div>
+          <div className="cupboard_flexbox_middle">
+            <div className="cupboard_single_flexbox_middle_first">
+              <img
+                src={"images/hoodie.png"}
+                alt="hoodie"
+                style={{
+                  height: "27.5vh",
+                  width: "19vw",
+                  borderTopLeftRadius: "15%",
+                  borderTopRightRadius: "15%",
+                }}
+              ></img>
+            </div>
+            <div className="cupboard_single_flexbox_middle_second">
+              <div className="cupboard_flexbox_name_date">
+                <div className="cupboard_single_name">
+                  <p>Lorem Ipsum</p>
+                </div>
+                <div className="cupboard_single_date">
+                  <p>01.01.2023</p>
+                </div>
+              </div>
+              <div className="cupboard_single_heart">
+                <img
+                  src={"cupboard/Icon awesome-heart.svg"}
+                  alt="heart"
+                  style={{ height: "3.5vh", width: "3.5vh" }}
+                ></img>
+              </div>
+            </div>
+            <div className="cupboard_single_flexbox_middle_third">
+              <div className="delete_single_flexbox">
+                <p>DELETE</p>
+              </div>
+              <div className="edit_single_flexbox">
+                <p>EDIT</p>
+              </div>
+            </div>
+          </div>
+          <div className="cupboard_flexbox_middle">
+            <div className="cupboard_single_flexbox_middle_first">
+              <img
+                src={"images/hoodie.png"}
+                alt="hoodie"
+                style={{
+                  height: "27.5vh",
+                  width: "19vw",
+                  borderTopLeftRadius: "15%",
+                  borderTopRightRadius: "15%",
+                }}
+              ></img>
+            </div>
+            <div className="cupboard_single_flexbox_middle_second">
+              <div className="cupboard_flexbox_name_date">
+                <div className="cupboard_single_name">
+                  <p>Lorem Ipsum</p>
+                </div>
+                <div className="cupboard_single_date">
+                  <p>01.01.2023</p>
+                </div>
+              </div>
+              <div className="cupboard_single_heart">
+                <img
+                  src={"cupboard/Icon awesome-heart.svg"}
+                  alt="heart"
+                  style={{ height: "3.5vh", width: "3.5vh" }}
+                ></img>
+              </div>
+            </div>
+            <div className="cupboard_single_flexbox_middle_third">
+              <div className="delete_single_flexbox">
+                <p>DELETE</p>
+              </div>
+              <div className="edit_single_flexbox">
+                <p>EDIT</p>
+              </div>
+            </div>
+          </div>
+          <div className="cupboard_flexbox_middle">
+            <div className="cupboard_single_flexbox_middle_first">
+              <img
+                src={"images/hoodie.png"}
+                alt="hoodie"
+                style={{
+                  height: "27.5vh",
+                  width: "19vw",
+                  borderTopLeftRadius: "15%",
+                  borderTopRightRadius: "15%",
+                }}
+              ></img>
+            </div>
+            <div className="cupboard_single_flexbox_middle_second">
+              <div className="cupboard_flexbox_name_date">
+                <div className="cupboard_single_name">
+                  <p>Lorem Ipsum</p>
+                </div>
+                <div className="cupboard_single_date">
+                  <p>01.01.2023</p>
+                </div>
+              </div>
+              <div className="cupboard_single_heart">
+                <img
+                  src={"cupboard/Icon awesome-heart.svg"}
+                  alt="heart"
+                  style={{ height: "3.5vh", width: "3.5vh" }}
+                ></img>
+              </div>
+            </div>
+            <div className="cupboard_single_flexbox_middle_third">
+              <div className="delete_single_flexbox">
+                <p>DELETE</p>
+              </div>
+              <div className="edit_single_flexbox">
+                <p>EDIT</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="cupboard_flexbox_middle">
+            <div className="cupboard_single_flexbox_middle_first">
+              <img
+                src={"images/hoodie.png"}
+                alt="hoodie"
+                style={{
+                  height: "27.5vh",
+                  width: "19vw",
+                  borderTopLeftRadius: "15%",
+                  borderTopRightRadius: "15%",
+                }}
+              ></img>
+            </div>
+            <div className="cupboard_single_flexbox_middle_second">
+              <div className="cupboard_flexbox_name_date">
+                <div className="cupboard_single_name">
+                  <p>Lorem Ipsum</p>
+                </div>
+                <div className="cupboard_single_date">
+                  <p>01.01.2023</p>
+                </div>
+              </div>
+              <div className="cupboard_single_heart">
+                <img
+                  src={"cupboard/Icon awesome-heart.svg"}
+                  alt="heart"
+                  style={{ height: "3.5vh", width: "3.5vh" }}
+                ></img>
+              </div>
+            </div>
+            <div className="cupboard_single_flexbox_middle_third">
+              <div className="delete_single_flexbox">
+                <p>DELETE</p>
+              </div>
+              <div className="edit_single_flexbox">
+                <p>EDIT</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="cupboard_flexbox_middle">
+            <div className="cupboard_single_flexbox_middle_first">
+              <img
+                src={"images/hoodie.png"}
+                alt="hoodie"
+                style={{
+                  height: "27.5vh",
+                  width: "19vw",
+                  borderTopLeftRadius: "15%",
+                  borderTopRightRadius: "15%",
+                }}
+              ></img>
+            </div>
+            <div className="cupboard_single_flexbox_middle_second">
+              <div className="cupboard_flexbox_name_date">
+                <div className="cupboard_single_name">
+                  <p>Lorem Ipsum</p>
+                </div>
+                <div className="cupboard_single_date">
+                  <p>01.01.2023</p>
+                </div>
+              </div>
+              <div className="cupboard_single_heart">
+                <img
+                  src={"cupboard/Icon awesome-heart.svg"}
+                  alt="heart"
+                  style={{ height: "3.5vh", width: "3.5vh" }}
+                ></img>
+              </div>
+            </div>
+            <div className="cupboard_single_flexbox_middle_third">
+              <div className="delete_single_flexbox">
+                <p>DELETE</p>
+              </div>
+              <div className="edit_single_flexbox">
+                <p>EDIT</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="sidebar_img">
+          <img
+            src={"sidebar/rectangle 161.svg"}
+            alt="menu"
+            style={{ height: "90vh" }}
+          />
+        </div>
+        <div className="sidebar">
+          <div className={"sidebar_menu" + " " + "sidebar_top"}>
+            <img src={"sidebar/menu.svg"} alt="menu"></img>
+          </div>
+          <div className={"sidebar_clothes" + " " + "sidebar_top"}>
+            <img src={"sidebar/wieszak.svg"} alt="menu"></img>
+          </div>
+          <div className={"sidebar_cupboard" + " " + "sidebar_top"}>
+            <img src={"sidebar/szafa.svg"} alt="menu"></img>
+          </div>
+          <div className={"sidebar_account" + " " + "sidebar_top"}>
+            <img src={"sidebar/moje konto.svg"} alt="menu"></img>
+          </div>
+          <div className={"sidebar_logout" + " " + "sidebar_bottom"}>
+            <img src={"sidebar/wyloguj.svg"} alt="menu"></img>
+          </div>
+        </div>
+      </body>
+    </>
+  );
+}
