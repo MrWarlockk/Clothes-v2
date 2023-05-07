965351a98770160d0433b3c8fdbef614e0da11a3
MrWarlockk <gnatmaciej@gmail.com>
2023-05-07 02:56:28 +0200

    Login site start
    Added the css file to the index site
    Made the background, title screen and login part

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,74 @@
 import Link from "next/link";
+import styles from "../styles/index.module.css";
+import classNames from "classnames";
 
 function Home() {
   return (
     <>
       <body>
-        <h1>Login page</h1>
-
+        <div className={styles.background_circle1}></div>
+        <div className={styles.background_circle2}></div>
+        <div className={styles.flexbox_main}>
+          <div className={styles.flexbox_main_img}>
+            <img src="index/your_personal_wardrobe.svg" alt="main_image"></img>
+          </div>
+          <div className={styles.flexbox_main_text}>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+              iaculis molestie augue ut gravida. Nunc lectus massa, pretium et
+              elit in, dapibus pharetra metus. Vestibulum non tempor nibh.
+              Phasellus tempus gravida pharetra. Fusce ornare elit eros, in
+              sollicitudin magna laoreet pharetra. Duis sed tortor sit amet nibh
+              ultricies tincidunt nec eget augue. Fusce maximus vulputate massa,
+              sed bibendum libero. Nullam erat lectus, volutpat sed felis ut,
+              volutpat euismod felis. Fusce non arcu sed lorem ultrices
+              sollicitudin.
+            </p>
+          </div>
+        </div>
+        <div className={styles.flexbox_login}>
+          <div
+            className={classNames(
+              styles.flexbox_login_item,
+              styles.flexbox_login_email
+            )}
+          >
+            E-MAIL
+          </div>
+          <div
+            className={classNames(
+              styles.flexbox_login_item,
+              styles.flexbox_login_password
+            )}
+          >
+            PASSWORD
+          </div>
+          <div
+            className={classNames(
+              styles.flexbox_login_item,
+              styles.flexbox_login_log_in
+            )}
+          >
+            LOG IN
+          </div>
+          <div
+            className={classNames(
+              styles.flexbox_login_item,
+              styles.flexbox_login_text1
+            )}
+          >
+            Forgot your password?
+          </div>
+          <div
+            className={classNames(
+              styles.flexbox_login_item,
+              styles.flexbox_login_text2
+            )}
+          >
+            Join Here
+          </div>
+        </div>
+        {/*
         <div className="sidebar">
           <Link href={"/menu"}>
             <div className={"sidebar_menu"}>
@@ -31,6 +94,7 @@ function Home() {
             <img src={"sidebar/wyloguj.svg"} alt="menu"></img>
           </div>
         </div>
+  */}
       </body>
     </>
   );
