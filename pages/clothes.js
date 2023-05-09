47a64fb8026a78c1c2e77efce3b40fd9fd7f183e
MrWarlockk <gnatmaciej@gmail.com>
2023-05-09 04:06:45 +0200

    Clothes page start
    Started making the clothes page
    Added the background
    Added the left side (need to make the images zoom instead of scale)
    Started working on the right side

--- pages/clothes.js
+++ pages/clothes.js
@@ -1,10 +1,41 @@
 import Link from "next/link";
+import styles from "../styles/clothes.module.css";
 
 function Clothes() {
   return (
     <>
       <body>
-        <h1>Clothes page</h1>
+        <div className={styles.background_white}></div>
+        <div className="background_line1"></div>
+        <div className="background_line2"></div>
+        <div className="background_line3"></div>
+        <div className={styles.background_circle1}></div>
+        <div className={styles.background_circle2}></div>
+        <div className={styles.outfit_creator}>
+          <div className={styles.outfit_creator_images}>
+            <img src={"clothes/hoodie.png"} alt="hoodie"></img>
+            <img src={"clothes/shirt.png"} alt="shirt"></img>
+            <img src={"clothes/pants.png"} alt="pants"></img>
+            <img src={"clothes/shoes.png"} alt="shoes"></img>
+          </div>
+          <div className={styles.outfit_creator_bottom}>
+            <div className={styles.outfit_creator_save}>SAVE OUTFIT</div>
+            <div className={styles.outfit_creator_generate}>
+              <div className={styles.outfit_creator_generate_text}>
+                GENERATE
+              </div>
+              <div className={styles.outfit_creator_generate_arrow_bg}>
+                <span className={styles.outfit_creator_generate_arrow}>
+                  &#9650;
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.clothes_menu}>
+          <div className={styles.clothes_menu_list}></div>
+          <div className={styles.clothes_menu_type}></div>
+        </div>
 
         <div className="sidebar">
           <Link href={"/menu"}>
