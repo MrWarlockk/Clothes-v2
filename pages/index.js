579edda10e5e49107725d9e3ed72adb786cf602a
MrWarlockk <gnatmaciej@gmail.com>
2023-05-14 00:40:05 +0200

    Sidebar login page

--- pages/index.js
+++ pages/index.js
@@ -174,33 +174,33 @@ function Home() {
             ></img>
           </div>
         </div>
-        {/*
-        <div className="sidebar">
-          <Link href={"/menu"}>
-            <div className={"sidebar_menu"}>
-              <img src={"sidebar/menu.svg"} alt="menu"></img>
+        {
+          <div className="sidebar">
+            <Link href={"/menu"}>
+              <div className={"sidebar_menu"}>
+                <img src={"sidebar/menu.svg"} alt="menu"></img>
+              </div>
+            </Link>
+            <Link href={"/clothes"}>
+              <div className={"sidebar_clothes"}>
+                <img src={"sidebar/wieszak.svg"} alt="menu"></img>
+              </div>
+            </Link>
+            <Link href={"/cupboard"}>
+              <div className={"sidebar_cupboard"}>
+                <img src={"sidebar/szafa.svg"} alt="menu"></img>
+              </div>
+            </Link>
+            <Link href={"/account"}>
+              <div className={"sidebar_account"}>
+                <img src={"sidebar/moje konto.svg"} alt="menu"></img>
+              </div>
+            </Link>
+            <div className={"sidebar_logout"}>
+              <img src={"sidebar/wyloguj.svg"} alt="menu"></img>
             </div>
-          </Link>
-          <Link href={"/clothes"}>
-            <div className={"sidebar_clothes"}>
-              <img src={"sidebar/wieszak.svg"} alt="menu"></img>
-            </div>
-          </Link>
-          <Link href={"/cupboard"}>
-            <div className={"sidebar_cupboard"}>
-              <img src={"sidebar/szafa.svg"} alt="menu"></img>
-            </div>
-          </Link>
-          <Link href={"/account"}>
-            <div className={"sidebar_account"}>
-              <img src={"sidebar/moje konto.svg"} alt="menu"></img>
-            </div>
-          </Link>
-          <div className={"sidebar_logout"}>
-            <img src={"sidebar/wyloguj.svg"} alt="menu"></img>
           </div>
-        </div>
-  */}
+        }
       </body>
     </>
   );
